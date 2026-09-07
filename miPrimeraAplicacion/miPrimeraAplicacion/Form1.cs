bf3578e5be5f25101ceae6c8b826793e678dd236
kev <kev@DESKTOP-N2CJVGN>
2026-09-06 21:38:09 -0600

    Creacion de la rama estadistica

--- miPrimeraAplicacion/miPrimeraAplicacion/Form1.cs
+++ miPrimeraAplicacion/miPrimeraAplicacion/Form1.cs
@@ -16,12 +16,5 @@ namespace miPrimeraAplicacion
         {
             InitializeComponent();
         }
-        
-        private void btnCalcular_Click(object sender, EventArgs e)
-        String[] serie = txtSerie.Text.Split(',');
 
-        ltsRespuesta.DataSource = serie.Select(n => int.Parse(n)).Where(n => n % 2 == 0)
-                .OrderBy(n=>n)
-                .ToList();
-    }
     }
